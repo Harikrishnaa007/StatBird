0aef89f1c5fa92afccf0e04f28c38aace7a30b9d
Harikrishnaa007 <75600089+Harikrishnaa007@users.noreply.github.com>
2024-11-13 19:30:27 +0530

    Create db.py

---
+++ app/db.py
@@ -0,0 +1,16 @@
+# app/db.py
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Initialize MongoDB client
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client["Statbird"]
+
+def get_collection():
+    return db["your_collection_name"]  # Modify this to your collection name
